ecfa4791d45c38c25fe7b0987cb6df74c3e978f1
Jose Marques <jf-marques@outlook.com>
2025-03-25 22:11:36 -0300

    Corrigindo nome container

--- 01-primeiro-container/run_containers.sh
+++ 01-primeiro-container/run_containers.sh
@@ -2,6 +2,6 @@
 docker pull nginx
 docker run -d -p 80:80 --name meu-servidor nginx:latest
 docker container ls
-docker container stop $(docker container ls -qa)
-docker container rm $(docker container ls -qa)
+docker container stop meu-servidor
+docker container rm meu-servidor
 docker container ls
