091e22421665f925e4239784e3ab886f46011818
Jose Marques <jf-marques@outlook.com>
2025-04-18 11:22:35 -0300

    Ajustando script

--- 01-primeiro-container/run_containers.sh
+++ 01-primeiro-container/run_containers.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 docker pull nginx
 docker run -d -p 80:80 --name meu-servidor nginx:latest
+echo "Container em execução, acesse localhost para confirmar se funcionou!"
 docker container ls
+read -p "Tecle Enter para encerrar"
 docker container stop meu-servidor
 docker container rm meu-servidor
-docker container ls
+docker container ls -a
